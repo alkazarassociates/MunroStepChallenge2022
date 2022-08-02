f216a3a9181ba412952218f7479ea35d31900a11
Mike Elkins <mike@elkins.com>
2022-08-02 16:39:41 -0400

    Add working menu

--- msc_root/teams/views.py
+++ msc_root/teams/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Team
 
 def index(request):
-    context = {'team_list': Team.objects.order_by('name')}
+    context = {'team_list': Team.objects.order_by('name'), 'title': 'Munro Step Challenge Teams'}
     return render(request, 'teams/teams.html', context)

c692332a8862453ca8dd9f8cec345d8937095269
Mike Elkins <mike@elkins.com>
2022-08-05 11:53:53 -0400

    More flow, fix bad email

--- msc_root/mpc_groups/models.py
+++ msc_root/mpc_groups/models.py
@@ -12,7 +12,7 @@ class MpcGroup(models.Model):
 class MpcAdminRegistration(models.Model):
     name = models.CharField('Name', max_length=40, help_text="Enter the facebook name you use.")
     primary_group = models.CharField('Ambassador Group', max_length=60, help_text="Enter the name of your Ambassador Group as you wish it to appear.")
-    secondary_group = models.CharField('Optional Second Group', max_length=60, blank=True, help_text="If you are the admin of another group, you can enter it here to request your two groups being on the same Team.")
+    secondary_group = models.CharField('Optional Additional Group', max_length=60, blank=True, help_text="If you are the admin of another group, you can enter it here to request your two groups being on the same Team.")
 
     def __str__(self):
         sep = ''

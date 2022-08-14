879d0a13d2f11b5d22cd37408090a7d5ff28a601
Mike Elkins <mike@elkins.com>
2022-08-14 17:04:26 -0400

    Add modification log to admin interface

--- msc_root/mpc_groups/admin.py
+++ msc_root/mpc_groups/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import MpcAdminRegistration, MpcGroup
+from .models import MpcAdminRegistration, MpcGroup, GroupModifications
 
 # Register your models here.
 admin.site.register(MpcGroup)
 admin.site.register(MpcAdminRegistration)
+admin.site.register(GroupModifications)

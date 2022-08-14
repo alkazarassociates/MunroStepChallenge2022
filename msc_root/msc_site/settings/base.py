9d559a746f8cef8edb5bd685780955d4265f7837
Mike Elkins <mike@elkins.com>
2022-08-14 11:50:27 -0400

    Move to Postgres

--- msc_root/msc_site/settings/base.py
+++ msc_root/msc_site/settings/base.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'msc_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'team_step_challenge_2022',
+        'USER': 'postgres',
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

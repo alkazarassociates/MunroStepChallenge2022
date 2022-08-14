9a5dc69fd215f6fc9527bf565d6192d93e78870c
Mike Elkins <mike@elkins.com>
2022-08-14 17:14:29 -0400

    Display updated time

--- msc_root/mpc_groups/views.py
+++ msc_root/mpc_groups/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from .forms import MpcAdminRegistrationForm
-from .models import MpcGroup
+from .models import GroupModifications, MpcGroup
 
 def index(request):
-    context = {'group_list': MpcGroup.objects.order_by('name')}
+    context = {
+        'group_list': MpcGroup.objects.order_by('name'),
+        'updated': GroupModifications.objects.latest().modification_time.strftime("%m/%d/%Y %h:%m"),
+    }
     return render(request, 'mpc_groups/mpc_groups.html', context)
 
 def register(request):

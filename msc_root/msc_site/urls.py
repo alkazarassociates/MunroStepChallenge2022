ee4733e7f3a48d416e1004b062e4df74446385ef
Mike Elkins <mike@elkins.com>
2022-08-15 20:46:44 -0400

    Basic user registration

--- msc_root/msc_site/urls.py
+++ msc_root/msc_site/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+from steps.views import Register
 
 urlpatterns = [
     #path('steps/', include('steps.urls')),
     path('admin/', admin.site.urls),
+    path('register/success/', TemplateView.as_view(template_name='registration/success.html'), name='register-success'),
+    path('register/', Register.as_view(), name='register'),
+    path('', include('django.contrib.auth.urls')),
     path('', include('landing.urls')),
     #path('teams', include('teams.urls')),
     path('groups/', include('mpc_groups.urls')),
--- msc_root/steps/views.py
+++ msc_root/steps/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 from .models import StepEntry
 from .forms import StepEntryForm
+
+class Register(CreateView):
+    template_name: str = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register-success')
+
+    def form_valid(self, form):
+        form.save()
+        return HttpResponseRedirect(self.success_url)
 
 def step_entry(request):
     submitted = False

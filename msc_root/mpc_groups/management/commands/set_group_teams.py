6e6e1b07bd8735d7ab1f48a8fda942bf4ca3997c
Mike Elkins <mike@elkins.com>
2022-08-29 19:03:57 -0400

    Assign ungrouped peakers

--- msc_root/mpc_groups/management/commands/set_group_teams.py
+++ msc_root/mpc_groups/management/commands/set_group_teams.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import json
 
 from django.core.management.base import BaseCommand
@@ -19,8 +20,20 @@ class Command(BaseCommand):
             group.team = Team.objects.get(name=assignments[g])
             group.save()
         print()
+        counter = defaultdict(int)
         for peaker in User.objects.all():
             if hasattr(peaker, 'profile') and peaker.profile.group:
                 print(f"Peaker {peaker.username} in {peaker.profile.group} to Team {peaker.profile.group.team}")
                 peaker.profile.team = peaker.profile.group.team
+                counter[peaker.profile.team] += 1
                 peaker.save()
+        for peaker in User.objects.all():
+            if hasattr(peaker, 'profile'):
+                if not peaker.profile.team or not peaker.profile.group:
+                    best_team = min(counter, key=counter.get)
+                    peaker.profile.team = best_team
+                    print(f"Peaker {peaker.username} (unassigned) to Team {peaker.profile.team}")
+                    counter[peaker.profile.team] += 1
+                    peaker.save()
+        for t in counter:
+            print(f"Team {t}: {counter[t]}")

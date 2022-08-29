50f1caf05fc6a5fba9ef9f043f533a7461f9c7a9
Mike Elkins <mike@elkins.com>
2022-08-29 19:12:46 -0400

    Add new peakers to smallest group

--- msc_root/steps/views.py
+++ msc_root/steps/views.py
@@ -10,6 +10,7 @@ import datetime
 from .models import Profile, StepEntry
 from .forms import PeakerRegistrationForm, StepEntryForm, PeakerModificationForm
 from mpc_groups.models import MpcGroup
+from teams.models import Team
 
 class Register(CreateView):
             
@@ -24,6 +25,13 @@ class Register(CreateView):
         prof.group=form.cleaned_data['group_field']
         if prof.group and prof.group.team:
             prof.team = prof.group.team
+        if not prof.team:
+            # We need to calculate what team to put this peaker on.
+            counter = {}
+            for team in Team.objects.all():
+                counter[team] = User.objects.filter(profile__team=team).count()
+            prof.team = min(counter, key=counter.get)
+
         prof.save()
         return HttpResponseRedirect(self.success_url)
 

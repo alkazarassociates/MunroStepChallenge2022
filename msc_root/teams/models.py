e06a0c593b21b207c0f24cb3ca473120c6b20b9b
Mike Elkins <mike@elkins.com>
2022-08-29 16:21:23 -0400

    Color code teams

--- msc_root/teams/models.py
+++ msc_root/teams/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField('Name', max_length=16, unique=True)
+    description = models.TextField('Description')
+    color = models.CharField('Color', max_length=20, default='Black')
 
     def __str__(self):
         return self.name

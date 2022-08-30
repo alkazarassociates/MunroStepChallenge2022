ca29e4149fd3fac9f9b22d757eb53c2e963678a9
Mike Elkins <mike@elkins.com>
2022-08-30 18:08:25 -0400

    Add total steps on home page

--- msc_root/landing/views.py
+++ msc_root/landing/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from steps.models import StepEntry
 
 def index(request):
-    return render(request, 'landing/landing.html')
+    total = 0
+    if request.user.is_authenticated:
+        total = StepEntry.objects.filter(peaker=request.user).aggregate(Sum('steps'))['steps__sum']
+    return render(request, 'landing/landing.html', {'total_steps': total})

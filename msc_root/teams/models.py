9e8a5cbced2e6f475a5d64d54f47a7fb023a319d
Mike Elkins <mike@elkins.com>
2022-08-30 12:49:59 -0400

    Add Tree images, background

--- msc_root/teams/models.py
+++ msc_root/teams/models.py
@@ -4,6 +4,7 @@ class Team(models.Model):
     name = models.CharField('Name', max_length=16, unique=True)
     description = models.TextField('Description')
     color = models.CharField('Color', max_length=20, default='Black')
+    picture = models.CharField('Picture', max_length=40, blank=True, null=True)
 
     def __str__(self):
         return self.name

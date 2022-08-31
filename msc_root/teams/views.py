8145d5c772c66f707d8e098fe19cba607f77ab17
Mike Elkins <mike@elkins.com>
2022-08-30 22:32:52 -0400

    List groups in the team pages

--- msc_root/teams/views.py
+++ msc_root/teams/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from mpc_groups.models import MpcGroup
 from .models import Team
 
 def index(request):
@@ -6,5 +7,13 @@ def index(request):
     return render(request, 'teams/teams.html', context)
 
 def team_page(request, args):
-    context = {'team': Team.objects.get(name=args)}
+    team = Team.objects.get(name=args)
+    group_list = MpcGroup.objects.filter(team=team).order_by('name')
+    groups = [group.name for group in group_list]
+    while len(groups) % 3 != 0:
+        groups.append('')
+    group_lines = []
+    for i in range(len(groups) // 3):
+        group_lines.append([groups[3*i], groups[3*i+1], groups[3*i+2]])
+    context = {'team': team, 'group_list': group_lines}
     return render(request, 'teams/team_page.html', context )

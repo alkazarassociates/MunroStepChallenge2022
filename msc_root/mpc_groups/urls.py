755a2ea32bce8a43cb3209c18b81d5adf305c583
Mike Elkins <mike@elkins.com>
2022-08-31 19:07:42 -0400

    Add link to member list to groups

--- msc_root/mpc_groups/urls.py
+++ msc_root/mpc_groups/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('register/', views.register),
-    re_path(r'^(?P<group>[\w ]+)$', views.members),
+    re_path(r'^(?P<group>[\w\&\,/ -]+)$', views.members, name='group_page'),
     path('', views.index, name='groups')
 ]

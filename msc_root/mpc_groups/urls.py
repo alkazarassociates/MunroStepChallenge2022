138e88691cfa9ceeecf735a1853c0cc564fcde7e
Mike Elkins <mike@elkins.com>
2022-08-31 15:48:40 -0400

    Take out group registration

--- msc_root/mpc_groups/urls.py
+++ msc_root/mpc_groups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register),
+    # path('register/', views.register),
     re_path(r'^(?P<group>[\w ]+)$', views.members),
     path('', views.index, name='groups')
 ]

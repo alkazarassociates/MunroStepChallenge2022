f07c1611896d5e0029585635f92c56e1f7faa251
Mike Elkins <mike@elkins.com>
2022-09-01 18:37:28 -0400

    Fix missing import

--- msc_root/steps/management/commands/broken_users.py
+++ msc_root/steps/management/commands/broken_users.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from steps.models import Profile
+from teams.models import Team
 
 class Command(BaseCommand):
     help = 'Set Group Teams'

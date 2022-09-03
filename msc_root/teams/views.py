3f6b6d386b846de110d6481ee8e9c0855c7340e6
Mike Elkins <mike@elkins.com>
2022-09-03 16:47:20 -0400

    Add reports to teams

--- msc_root/teams/views.py
+++ msc_root/teams/views.py
@@ -9,19 +9,44 @@ def index(request):
     context = {'team_list': Team.objects.order_by('name'), 'title': 'Munro Step Challenge'}
     return render(request, 'teams/teams.html', context)
 
+def nb(text):
+    return text.replace(' ', "&nbsp;")
+
+def amp(text):
+    return text.replace(' & ', '&amp;')
+
+def pp(text):
+    return nb(amp(text))
+
 def team_page(request, args):
     team = Team.objects.get(name=args)
     group_list = MpcGroup.objects.filter(team=team).order_by('name')
-    groups = [group.name for group in group_list]
+    groups = [pp(group.name) for group in group_list]
     while len(groups) % 3 != 0:
         groups.append('')
     group_lines = []
     for i in range(len(groups) // 3):
         group_lines.append([groups[3*i], groups[3*i+1], groups[3*i+2]])
-    team_steps = StepEntry.objects.filter(peaker__profile__team=team).aggregate(Sum('steps'))['steps__sum']
-    if team_steps is None:
-        team_steps = 0
+
+    dates = StepEntry.objects.distinct('date').values_list('date')
+    day_totals = []
+    step_total = 0
+    distance_total = 0
+    unit_name = 'Kilometers'
+    conv = 0.7242 / 1000.0
+    if hasattr(request.user, 'profile') and request.user.profile.imperial:
+        unit_name = 'Miles'
+        conv = 0.45 / 1000.0
+    for date in dates:
+        day = date[0]
+        steps = StepEntry.objects.filter(date=day, peaker__profile__team=team).aggregate(Sum('steps'))['steps__sum'] or 0
+        step_total += steps
+        distance = conv * steps
+        distance_total += distance
+        day_totals.append({'day': day, 'steps': steps, 'distance': distance})
+    day_totals.append({'day': 'Total', 'steps': step_total, 'distance': distance_total})
+
     context = {
         'team': team, 'group_list': group_lines, 'team_size': User.objects.filter(profile__team=team).count(), 
-        'team_steps': team_steps}
+        'team_steps': step_total, 'day_totals': day_totals, 'unit_name': unit_name}
     return render(request, 'teams/team_page.html', context )

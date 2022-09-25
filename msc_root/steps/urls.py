a47fdc9c626c39aa552e522fc7e1c9548e5854d6
Mike Elkins <mike@elkins.com>
2022-09-25 12:22:20 -0400

    Another missing legal character...

--- msc_root/steps/urls.py
+++ msc_root/steps/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('large_entry', views.large_entry, name='large_entry'),
     path('overwrite', views.overwrite_confirm, name='overwrite_confirm'),
     path('report', views.step_report, name='step_report'),
-    re_path(r'^report/(?P<peaker_name>[\w%.]+)/$', views.step_report, name='step_report'),
+    re_path(r'^report/(?P<peaker_name>[\w%.-]+)/$', views.step_report, name='step_report'),
     path('admin', views.admin_report, name='admin_report'),
     path('', views.step_entry, name='step_entry'),
 ]

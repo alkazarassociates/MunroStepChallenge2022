2644fa9e5eb80cfee58d3b4ce74a9bf9a9477061
Mike Elkins <Mike@Elkins.com>
2023-08-26 12:37:45 -0400

    salted_hmac

--- msc_root/steps/tokens.py
+++ msc_root/steps/tokens.py
@@ -1,12 +1,16 @@
 # From https://www.javatpoint.com/django-user-registration-with-email-confirmation
 from django.contrib.auth.tokens import PasswordResetTokenGenerator  
+from django.utils.crypto import salted_hmac
 
 class TokenGenerator(PasswordResetTokenGenerator):  
     def _make_hash_value(self, user, timestamp):
         print(f"Make Hash {user.pk} {timestamp} {user.is_active}")
-        print(f" salt={self.key_salt} sec={self.secret} alg={self.algorithm}")
-        return (  
+        ret = (  
             str(user.pk) + str(timestamp) +  
             str(user.is_active)  
         )  
+        signature = salted_hmac(self.key_salt, ret, secret=self.secret, algorithm=self.algorithm)
+        print(signature)
+        return ret
+    
 account_activation_token = TokenGenerator() 

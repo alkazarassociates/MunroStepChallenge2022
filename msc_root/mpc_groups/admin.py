930b64dc22e4a8a8efb6cd2d1adeab0514b1805a
Mike Elkins <Mike@Elkins.com>
2023-08-28 12:16:38 -0400

    Add more data to group admin interface

--- msc_root/mpc_groups/admin.py
+++ msc_root/mpc_groups/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import MpcAdminRegistration, MpcGroup, GroupModifications
 
 # Register your models here.
-admin.site.register(MpcGroup)
+class MpcGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team', 'admin')
+
+admin.site.register(MpcGroup, MpcGroupAdmin)
 admin.site.register(MpcAdminRegistration)
 admin.site.register(GroupModifications)

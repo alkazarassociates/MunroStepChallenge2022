ca30d67dc7da3266ca46f1f35c384432e030eb78
Mike Elkins <Mike@Elkins.com>
2023-08-29 10:59:37 -0400

    Make FAQs work

--- msc_root/faq_page/views.py
+++ msc_root/faq_page/views.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.shortcuts import render
 
 from .models import FaqPage
 
 def faq_view(request):
-    text = FaqPage.objects.first()
-    return render(request, 'faq_page/faq.html', {'text': text})
+    return render(request, 'faq_page/faq.html', {'faqs': FaqPage.objects.order_by('id'), 'phase': settings.CURRENT_PHASE})

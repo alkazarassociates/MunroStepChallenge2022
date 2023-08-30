d85d3b9930cd8fd87051f7980c0f5e1431299786
Mike Elkins <Mike@Elkins.com>
2023-08-30 16:10:26 -0400

    Add Italian

--- msc_root/msc_site/settings/base.py
+++ msc_root/msc_site/settings/base.py
@@ -125,7 +125,8 @@ USE_L10N = True
 USE_TZ = True
 
 LANGUAGES = [('en', 'English'),
-              ('no', 'Norwegian')]
+              ('no', 'Norwegian'),
+              ('it', 'Italian')]
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 # Static files (CSS, JavaScript, Images)

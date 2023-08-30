9fc28922ea18670a7b2c1ef616cf94d666355e4d
Mike Elkins <Mike@Elkins.com>
2023-08-30 15:54:16 -0400

    Check in translations, enable Norwegian

--- msc_root/msc_site/settings/base.py
+++ msc_root/msc_site/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,6 +124,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = [('en', 'English'),
+              ('no', 'Norwegian')]
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/

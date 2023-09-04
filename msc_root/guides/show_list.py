9c50a5cc14c8d206c12387b83849568bb114851e
Mike Elkins <Mike@Elkins.com>
2023-09-04 13:15:33 -0400

    Make lists more pretty

--- msc_root/guides/show_list.py
+++ msc_root/guides/show_list.py
@@ -1,7 +1,7 @@
 SHOW_LIST = [
     'Battlestar Galactica (2005)',
     'Babylon 5',
-    'Star Trek (choose your flavor, there are 26  https://en.wikipedia.org/wiki/Template:Star_Trek_abbreviations)',
+    'Star Trek (choose your flavor, there are <a href="https://en.wikipedia.org/wiki/Template:Star_Trek_abbreviations">26</a>)',
     'For All Mankind',
     'Flash Gordon',
     'Futurama',

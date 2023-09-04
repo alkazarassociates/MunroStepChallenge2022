08525ffaa6ff050ab7e45749d793700fa4c89c22
Mike Elkins <Mike@Elkins.com>
2023-09-04 12:59:46 -0400

    Add movie and show lists

---
+++ msc_root/guides/show_list.py
@@ -0,0 +1,11 @@
+SHOW_LIST = [
+    'Battlestar Galactica (2005)',
+    'Babylon 5',
+    'Star Trek (choose your flavor, there are 26  https://en.wikipedia.org/wiki/Template:Star_Trek_abbreviations)',
+    'For All Mankind',
+    'Flash Gordon',
+    'Futurama',
+    'Firefly',
+    'The Expanse',
+    'Dr Who',
+]
--- msc_root/guides/views.py
+++ msc_root/guides/views.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.shortcuts import render
+from .movie_list import MOVIE_LIST
+from .show_list import SHOW_LIST
+
 
 def MpcMunroGuide(request):
     return render(request, 'guides/mpc_munro_guide.html', {'phase': settings.CURRENT_PHASE})
@@ -8,7 +11,7 @@ def index(request):
     return render(request, 'guides/index.html', {'phase': settings.CURRENT_PHASE})
 
 def movies(request):
-    return render(request, 'guides/list.html', {'phase': settings.CURRENT_PHASE, 'title': 'Movies to Inspire', 'list_items': ['sample']})
+    return render(request, 'guides/list.html', {'phase': settings.CURRENT_PHASE, 'title': 'Movies to Inspire', 'list_items': MOVIE_LIST})
 
 def shows(request):
-    return render(request, 'guides/list.html', {'phase': settings.CURRENT_PHASE, 'title': 'TV Shows to Inspire', 'list_items': ['sample2']})
+    return render(request, 'guides/list.html', {'phase': settings.CURRENT_PHASE, 'title': 'TV Shows to Inspire', 'list_items': SHOW_LIST})

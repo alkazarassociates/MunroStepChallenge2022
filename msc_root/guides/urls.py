b202ff39aa702dea8221faa59ea563733a0048e4
Mike Elkins <Mike@Elkins.com>
2023-09-05 10:54:51 -0400

    Rough draft of Hidden Figures

--- msc_root/guides/urls.py
+++ msc_root/guides/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('mpc_munro', views.MpcMunroGuide, name='mpc_guide'),
     path('movies', views.movies),
     path('shows', views.shows),
+    path('team_notes', views.team_notes, name="team_notes"),
     path('', views.index, name='index'),
 ]
--- msc_root/guides/views.py
+++ msc_root/guides/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.shortcuts import render
 from .movie_list import MOVIE_LIST
 from .show_list import SHOW_LIST
+from teams.models import Team
 
 
 def MpcMunroGuide(request):
@@ -15,3 +16,7 @@ def movies(request):
 
 def shows(request):
     return render(request, 'guides/list.html', {'phase': settings.CURRENT_PHASE, 'title': 'TV Shows to Inspire', 'list_items': SHOW_LIST})
+
+def team_notes(request):
+    context = {'team_list': Team.objects.order_by('name'), 'title': 'Our Hidden Figures', 'phase': settings.CURRENT_PHASE}
+    return render(request, 'guides/team_notes.html', context)

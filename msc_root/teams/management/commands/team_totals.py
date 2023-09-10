9ce390621207c1a20657e51be3ca58f16f760cad
Mike Elkins <Mike@Elkins.com>
2023-09-10 09:19:53 -0400

    Add group output to team_totals

--- msc_root/teams/management/commands/team_totals.py
+++ msc_root/teams/management/commands/team_totals.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.core.management.base import BaseCommand
 from django.db.models import Sum
+from mpc_groups.models import MpcGroup
 from steps.models import StepEntry
 from teams.models import Team
 
@@ -14,9 +15,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         today = datetime.date.today()
         cutoff = datetime.date(today.year, today.month, options['day'])
+        print(f"Total of steps on or before {cutoff} as entered at {datetime.datetime.now()}")
         all_teams_total = 0
         for team in Team.objects.order_by('name'):
-            step_total = StepEntry.objects.filter(date__lte=cutoff, peaker__profile__team=team).aggregate(Sum('steps'))['steps__sum']
+            step_total = StepEntry.objects.filter(date__lte=cutoff, peaker__profile__team=team).aggregate(Sum('steps'))['steps__sum'] or 0
             print(f"Team {team.name},{step_total}")
             all_teams_total += step_total
         print(f"ALL,{all_teams_total}")
+        print()
+        for group in MpcGroup.objects.order_by('name'):
+            step_total = StepEntry.objects.filter(date__lte=cutoff, peaker__profile__group=group).aggregate(Sum('steps'))['steps__sum'] or 0
+            print(f"{group.name},{step_total}")

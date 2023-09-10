d850320594b0286a27aa0164381e0fedbafddfee
Mike Elkins <Mike@Elkins.com>
2023-09-09 20:14:17 -0400

    Add TeamTotal tool

---
+++ msc_root/teams/management/commands/team_totals.py
@@ -0,0 +1,22 @@
+import datetime
+
+from django.core.management.base import BaseCommand
+from django.db.models import Sum
+from steps.models import StepEntry
+from teams.models import Team
+
+class Command(BaseCommand):
+    help = 'Print Team Totals'
+
+    def add_arguments(self, parser) -> None:
+        parser.add_argument('day', type=int)
+
+    def handle(self, *args, **options):
+        today = datetime.date.today()
+        cutoff = datetime.date(today.year, today.month, options['day'])
+        all_teams_total = 0
+        for team in Team.objects.order_by('name'):
+            step_total = StepEntry.objects.filter(date__lte=cutoff, peaker__profile__team=team).aggregate(Sum('steps'))['steps__sum']
+            print(f"Team {team.name},{step_total}")
+            all_teams_total += step_total
+        print(f"ALL,{all_teams_total}")

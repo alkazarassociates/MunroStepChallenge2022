9c2b4c343ea9f3bdd1dd18a6d5ccd8d069debf7c
Mike Elkins <Mike@Elkins.com>
2023-09-15 10:00:54 -0400

    Admin quality of life

--- msc_root/steps/admin.py
+++ msc_root/steps/admin.py
@@ -10,8 +10,16 @@ class StepEntryAdmin(admin.ModelAdmin):
 admin.site.register(StepEntry, StepEntryAdmin)
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('peaker', 'group', 'team')
+    list_display = ('peaker', 'get_email', 'get_active', 'group', 'team')
     list_filter = ('group', 'team')
-    search_fields = ('peaker__username',)
+    search_fields = ('peaker__username', 'peaker__email')
+
+    def get_email(self, obj):
+        return obj.peaker.email
+    get_email.short_description = 'Email'
+    
+    def get_active(self, obj):
+        return obj.peaker.is_active
+    get_active.short_description = 'Active'
 
 admin.site.register(Profile, ProfileAdmin)
